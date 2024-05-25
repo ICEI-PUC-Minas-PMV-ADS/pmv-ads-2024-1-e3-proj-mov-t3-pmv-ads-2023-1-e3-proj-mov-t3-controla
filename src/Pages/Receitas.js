3416dd8baf3fb83bede05a8c16de1c04edadf975
Marco Antonio Gil <gildo_marco@hotmail.com>
2024-05-25 10:54:54 -0300

    confirmando merge

--- src/Pages/Receitas.js
+++ src/Pages/Receitas.js
@@ -1,49 +1,4 @@
 import React from 'react';
-<<<<<<< HEAD
-import {View, StyleSheet, Text, Image} from 'react-native';
-import Form from '../components/FormReceitas/index';
-
-const Receitas = () => {
-  return (
-    <View style={styles.container}>
-     <View style={styles.imagem}>
-        <Image
-              source={require('../assets/Logo.png')}
-              style={styles.logo}
-              resizeMode="contain"
-        />
-        <View style={styles.divider} />
-      </View>
-      <View style={styles.header}>  
-        <Form />
-      </View>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#06358f' // Cor de fundo geral
-  },
-  imagem: {
-    alignItems: 'center'
-  },
-  header: {
-    padding: 10
-  },
-  logo: {
-    width: 225,
-    height: 83,
-  },
-  divider: {
-    width: '95%', // Largura da linha
-    height: 1, // Altura da linha
-    backgroundColor: '#d7d7d7e8', // Cor da linha (branca)
-    marginBottom: 20, // Espaçamento abaixo da linha
-  }
-});
-=======
 import Form from '../components/FormReceitas/index';
 import Container from '../components/Container';
 
@@ -55,6 +10,5 @@ const Receitas = () => {
   );
 };
 
->>>>>>> gildoamaral
 
 export default Receitas;
